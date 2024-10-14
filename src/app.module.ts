e6ea68d8960453cf4ff4eeee24fe2046d718fd44
chetanananthu <chetanananthu@gmail.com>
2024-10-14 19:33:35 +0530

    nestjs-tutorial

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import{User} from './user/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'pass@word1',
+      database: 'nestcrud',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule, // Add the UserModule to the imports array
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
